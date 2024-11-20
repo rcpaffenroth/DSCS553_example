c471f5b5703cda6b0ea3ba509c2e47eec496939e
Randy Paffenroth <randy.paffenroth@gmail.com>
2024-11-20 12:37:28 -0500

    Script for submitting sweeps to sbatch

---
+++ turing_example/sbatch_sweep_demo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# The partition we want (short==24 hours, long=7 days)
+#SBATCH --partition short
+# One node
+#SBATCH -N 1
+# One job on that node
+#SBATCH -n 1
+# Please give me a GPU
+##SBATCH --gres=gpu
+# Give a CPU from the following list
+##SBATCH --constraint="A100"
+
+# Ask for memory
+#SBATCH --mem=16gb
+
+# Get a node for more general use.
+
+# Run a python program using our local virtual environment
+cd /home/rcpaffenroth/DS553/CS553_example/turing_example
+venv/bin/wandb agent rcpaffenroth-wpi/cs553-turing-example/7rnxbz9h
+
