ddf5e3cd3b495d0bfae115d659038a7e3d3048ca
Randy Paffenroth <randy.paffenroth@gmail.com>
2025-09-22 07:46:29 -0400

    Small changes

--- red_team_v1.sh
+++ red_team_v1.sh
@@ -6,15 +6,14 @@ num_files=40
 # vars
 PORT=22000
 MACHINE=paffenroth-23.dyn.wpi.edu
+KEY=$HOME/projects/1_classes/DS553_private/scripts/CS2/student-admin_key
 
 # Loop to create files
 for i in $(seq 1 $num_files); do
-    echo "trying group ${i} at port $((${i}+${PORT})) "
-    echo "------------------------------------------------"
-    echo "------------------------------------------------"
-    ssh -i ../../scripts/student-admin_key -p $((${i}+${PORT})) -o StrictHostKeyChecking=no student-admin@${MACHINE} hostname
-    echo "------------------------------------------------"
-    echo "------------------------------------------------"    
+  echo "trying group ${i} at port $((${i} + ${PORT})) "
+  echo "------------------------------------------------"
+  echo "------------------------------------------------"
+  ssh -i $KEY -p $((${i} + ${PORT})) -o StrictHostKeyChecking=no student-admin@${MACHINE} hostname
+  echo "------------------------------------------------"
+  echo "------------------------------------------------"
 done
-
-
